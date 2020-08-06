a236b3e24ce98787a96e0b2e2ad82719d9c2f95b
Lee <lcs625@naver.com>
2020-08-06 14:43:02 +0900

    08/06

--- File_py/temp.py
+++ File_py/temp.py
@@ -16,20 +16,3 @@ from word_prepro import word_preprocessing
 temp_r = word_preprocessing('서버 개발자')
 print(type(temp_r))
 
-
-# #임시
-
-
-# client = MongoClient('mongodb://172.17.0.2:27017/')
-# mydb = client['job_opening']
-# mycol = mydb["opening_data"]
-
-# tlst = mycol.find({"Detail_Category": "서버 개발자"})
-# # select_col = s_col  # 1.MainTask 2.Qual, 3.Qual2
-
-# templst = []
-# for x in tlst:
-#     templst.append(x)
-
-# tempdf = pd.DataFrame(templst)
-# print(tempdf)
